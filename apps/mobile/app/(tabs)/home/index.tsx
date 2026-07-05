cfc8d345889bcbb9b8703d9a1439c6b5120d7101
kiuuon <kiuuon29@gmail.com>
2026-07-05 22:24:19 +0900

    feat(mobile): add Button component

--- apps/mobile/app/(tabs)/home/index.tsx
+++ apps/mobile/app/(tabs)/home/index.tsx
@@ -1,10 +1,15 @@
 import { View } from 'react-native';
+
 import Typography from '../../../components/common/Typography';
+import Button from '../../../components/common/Button';
 
 function Home() {
   return (
-    <View>
+    <View style={{ flex: 1, padding: 20 }}>
       <Typography.DataMono>1,708m</Typography.DataMono>
+      <Button variant="primary" size="lg" fullWidth onPress={() => {}}>
+        인증 확인
+      </Button>
     </View>
   );
 }
